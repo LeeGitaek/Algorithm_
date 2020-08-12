b6b4c3ee0e53b4a7dbc8c5d021327f3aba512d69
GitaekLee <coolfamily77@gmail.com>
2020-08-13 03:32:13 +0900

    백준 11047 => 동전 0

---
+++ BAEKJOON_PS/11047/11047.cpp
@@ -0,0 +1,28 @@
+//
+// Created by GiTaek Lee on 2020/08/13.
+//
+// 백준 11047 => 동전 0
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+
+    int n,k;
+    cin >> n >> k;
+    vector<int> a(n);
+    for(int i =0; i<n; i++)
+    {
+        cin >> a[i];
+    }
+    int ans = 0;
+    for(int i = n-1; n>=0; n--)
+    {
+        ans += k/a[i];
+        k %= a[i];
+    }
+
+    cout << ans << "\n";
+    return 0;
+}
